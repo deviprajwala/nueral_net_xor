ddde6aa60513fef32678b0895a6d03f59b64bf73
deviprajwala <deviprajwala3@gmail.com>
2021-03-04 17:41:18 +0530

    works perfectly comments need to be added

--- nueral_net_xor.cpp
+++ nueral_net_xor.cpp
@@ -99,20 +99,23 @@ float weight_update_internal_node (  vector < float > cal_outcome, float w_new ,
    
    return w_new;
 }
-float check (float val)
+float check_h2 (float val)
 {
-    float y;
-    if (val >= 0.05 && val <= 0.15 )
+    if (val > 0.5) 
     {
-        y = 1;
-    }
-    else
-    {
-        y = 0;
-    }
-    return y;
+      return 1;
+    } 
+    return 0;
 }
-void feed_forward_network (int count)
+float check_h1 ( float val )
+{
+    if(val > 0)
+    {
+        return 1;
+    }
+    return 0;
+}
+void feed_forward_network (int count )
 {
     int value = 0;
     w31 = 1, w41 = 1, w42 = 1, w23 = 1, w53 = 0.5, w54 = 0.5;
@@ -125,31 +128,36 @@ void feed_forward_network (int count)
    for( int i = 0; i < 4; i++ )
    {
        float val;
-       val = ( x1[i] * w31 ) + ( x2[i] * w23 );
-       y_n3.push_back(sigmoid_function (val ) );
+       val = ( -x1[i] * w31 ) + ( x2[i] * w23 );
+       val = check_h1( val );
+       y_n3.push_back (val );
+       
    }
    
    for(int i = 0; i < 4; i++)
    {
        float val;
-       val = ( x1[i] * w41 ) + ( x2[i] * w42 );
-       y_n4.push_back (sigmoid_function (val ) );
+       val = ( x1[i] * w41 ) + ( -x2[i] * w42 );
+       val = check_h1( val );
+       y_n4.push_back(val ) ;
    }
    
    for(int i = 0; i < 4; i++)
    {
        float val;
        val = ( y_n3[i] * w53 ) + ( y_n4[i] * w54 );
-       y_n5.push_back ( sigmoid_function (val ) );
+       val = check_h2(val);
+      // cout<<val<<" ";
+       y_n5.push_back (val );
    }
    for(int i = 0; i < 4; i++)
    {
       // cout<<y_n5[i]<<" ";
    }
   // cout<<"\n";
-   //cout<< error_function (y_n5)<<"\n ";
+  // cout<< error_function (y_n5)<<"\n ";
 
-   if ( error_function ( y_n5 ) > 0.4 )
+   if ( error_function ( y_n5 ) > 0.2 )
    {
       // weight_update_external_node(y_n5);
        w53 = weight_update_external_node ( y_n5, w53, y_n3 );
@@ -161,12 +169,13 @@ void feed_forward_network (int count)
        w42 = weight_update_internal_node ( y_n4, w42, y_n4, y_n5, w42, w23 );
        //weight_update_internal_node();
        
-     //  cout<<w53<<" "<<w54<<" "<<w31<<" "<<w41<<" "<<w23<<" "<<w42<<"\n";
-       if (value < count ) 
+      // cout<<w53<<" "<<w54<<" "<<w31<<" "<<w41<<" "<<w23<<" "<<w42<<"\n";
+
+       if( value < count)
        {
            value++;
            goto calculate;
-        
+
        }
    }
 
@@ -177,25 +186,27 @@ void predict(int x1 , int x2)
 
     float ans, y1, y2, y3;
 
-    y1 = ( x1 * w31 ) + ( x2 * w23 );
-    
-    y2 = ( x1 * w41 ) + ( x2 * w42 );
-
+    y1 = ( -x1 * w31 ) + ( x2 * w23 )  ;
+    y1 = check_h1 (y1);
+    y2 = ( x1 * w41 ) + ( -x2 * w42 ) ;
+    y2 = check_h1 (y2);
     y3 = ( sigmoid_function(y1) * w53 ) + ( sigmoid_function(y2) * w54 );
-
+    y3 = check_h2(y3);
    
-    cout<<sigmoid_function (y3)<<" ";
+    cout<<y3<<" ";
 }
 int main()
 {
     read_data();
     //write_data();
-    feed_forward_network(9);
+    feed_forward_network( 5 );
   //  feed_forward_network();
    // feed_forward_network();
-   predict(0,0);
+
+
+ predict(0,0);
      predict(0,1);
-       predict(1,0);
-         predict(1,1);
+     predict(1,0);
+        predict(1,1);
     return 0;
 }
